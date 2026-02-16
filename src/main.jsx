347eec519d1773874298e09e3e0475530b307728
themotugroup <themotugroupllc@gmail.com>
2026-02-16 16:10:54 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MotuGroupSite from "./MotuGroupSite.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MotuGroupSite />
+  </React.StrictMode>
+);
